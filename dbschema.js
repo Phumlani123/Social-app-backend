70961549a25ffcf4dd32d1437798d4abbd1eddb6
Phumlani <p.gwamanda@gmail.com>
2019-10-17 17:53:31 +0200

    save backend

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'scream body',
+            createdAt: '2019-10-13T18:00:59.974Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAiZaVWqaOomzZvq73aeVVLF9b4rviT36I",
+    authDomain: "social-app-62bf1.firebaseapp.com",
+    databaseURL: "https://social-app-62bf1.firebaseio.com",
+    projectId: "social-app-62bf1",
+    storageBucket: "social-app-62bf1.appspot.com",
+    messagingSenderId: "234360792441",
+    appId: "1:234360792441:web:ac7ee0564a4323bd187ba8"
+  };
